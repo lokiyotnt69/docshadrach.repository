ed3dc2ead8c76acbe032af4809ccd9cb85ab8ed6
Doc Shadrach <shadrachxbmc@gmail.com>
2014-08-14 16:57:27 +0200

    blank 1.1

--- zips/plugin.video.blank/main.py
+++ zips/plugin.video.blank/main.py
@@ -12,7 +12,7 @@ texto = str(file)
 tupla = texto.partition("XBMC")
 
 
-dialog.ok('Encuentra las instrucciones en:', str(tupla[0]), str(tupla[1]), str(tupla[2]))
+dialog.ok('Encuentra las instrucciones en:', str(tupla[0]), str(tupla[1]) + str(tupla[2]), "[COLOR ff708090]- - - - [I]version 1.1 (ver cambios en changelog.txt)[/I][/COLOR]")
 
 sys.exit()
 
